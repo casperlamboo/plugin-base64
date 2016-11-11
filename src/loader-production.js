942d1238ed7ef5732699a00ca2fee4cc15b390f4
casperlamboo <casperlamboo@gmail.com>
2016-11-11 11:15:49 +0100

    fix read file address

--- src/loader-production.js
+++ src/loader-production.js
@@ -1,9 +1,11 @@
+var isWindows = typeof process != 'undefined' && process.platform && process.platform.match(/^win/);
+
 var fs = require('fs');
 var mime = require('mime-types')
 
 exports.fetch = function(load) {
   return new Promise(function(resolve, reject) {
-    fs.readFile(load.address, function(error, result) {
+    fs.readFile(load.address.substr(isWindows ? 8 : 7), function(error, result) {
       if (error) {
         reject(error);
         return;
